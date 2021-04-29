b8d66117776d611b02ee81324b1886dc4497fd56
GZakhar88 <g.zakhar@gmail.com>
2021-04-29 14:05:25 +0200

    small correction in the write method

--- write.ts
+++ write.ts
@@ -7,8 +7,15 @@ const path: string = 'list.txt';
 export class Write {
 
     public writeMyList(inputText:string): void {
+        let listFromFile: string = fsync.readFileSync(path, 'utf-8');
+
         try{
-            fsync.writeFileSync(path,`\n${inputText}`, {flag: 'a'});
+            if(listFromFile.length === 0){
+                fsync.writeFileSync(path,inputText);
+            } else {
+                fsync.writeFileSync(path,`\n${inputText}`, {flag: 'a'});
+            };
+            
         } catch{
             console.log(`OOps something went wrong! Unable to write file: list.txt! Check if it is exist!`);
          };   

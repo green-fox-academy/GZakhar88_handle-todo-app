8b47af8050d443be64a564aca946fb41ed4c0d8d
GZakhar88 <g.zakhar@gmail.com>
2021-04-29 14:02:02 +0200

    No todos for today implemented

--- read.ts
+++ read.ts
@@ -1,13 +1,22 @@
 'use strict';
 
+import { connected } from "node:process";
+
 const fsync = require('fs');
 const path: string = 'list.txt';
 
 
 export class Read {
     public readMyList(): void {
-        try{
-            console.log(fsync.readFileSync(path, 'utf-8'));
+        try {
+            let listFromFile: string = fsync.readFileSync(path, 'utf-8');
+            
+            if(listFromFile.length === 0){
+                console.log('No todos for today! :)');
+            } else {
+                console.log(fsync.readFileSync(path, 'utf-8'));
+            }
+            
         } catch(error){
             console.log('Unable to read list. Check if it is exist!');
         }; 
